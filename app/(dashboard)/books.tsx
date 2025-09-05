854f9066b401215745b95638e11749b9f7d87578
Arturo Fonseca <arturofonsecas32@gmail.com>
2025-09-04 22:35:28 -0300

    feat: start developing book animations

--- app/(dashboard)/books.tsx
+++ app/(dashboard)/books.tsx
@@ -1,4 +1,12 @@
-import {FlatList, Pressable, StyleSheet, Text, View} from 'react-native'
+import {
+	Animated, Easing,
+	FlatList,
+	GestureResponderEvent,
+	Pressable,
+	StyleSheet,
+	Text,
+	View
+} from 'react-native'
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
 import ThemedView from "../../components/ThemedView";
@@ -6,16 +14,32 @@ import {globalStyle} from "../../styles";
 import {useBooks} from "../../hooks/useBooks";
 import ThemedCard from "../../components/ThemedCard";
 import {Colors} from "../../styles/colors";
-import {useEffect} from "react";
+import {useEffect, useRef, useState} from "react";
 import {useRouter} from "expo-router";
+import {Book} from "../../contexts/BooksContext";
 
 const Books = () => {
 	const {books, fetchBooks} = useBooks();
 	const router = useRouter();
+	const fadeAnim = useRef(new Animated.Value(0)).current;
+	const [pressedItem, setPressedItem] = useState<string>();
 
 	useEffect(() => {
 		fetchBooks();
 	}, []);
+
+	const expandBook = (event: GestureResponderEvent, item: Book)=> {
+		fadeAnim.setValue(0);
+		setPressedItem(item.$id);
+		Animated.timing(fadeAnim, {
+			toValue: -800,
+			duration: 500,
+			useNativeDriver: true,
+			easing: Easing.linear
+		}).start();
+		// event.target.measure();
+		// router.push(`/books/${item.$id}`)
+	}
 
 	return (
 		<ThemedView safe style={[globalStyle.container]}>
@@ -30,11 +54,24 @@ const Books = () => {
 				style={{width: '100%', marginTop: 40}}
         contentContainerStyle={styles.list}
 				renderItem={({item}) => (
-					<Pressable onPress={() => router.push(`/books/${item.$id}`)}>
-						<ThemedCard style={styles.card}>
-							<ThemedText style={globalStyle.title}>{item.title}</ThemedText>
-							<ThemedText>Written by {item.author}</ThemedText>
-						</ThemedCard>
+					<Pressable onPress={(event) => expandBook(event, item)}>
+
+						<Animated.View style={item.$id === pressedItem ? {
+							transform: [
+								{translateY: fadeAnim},
+								{perspective: 1000}
+							],
+							position: 'absolute',
+							 zIndex: 10000
+						}: undefined}>
+							<ThemedCard style={[styles.card, item.$id === pressedItem ? styles.pressed : undefined]}>
+
+									<ThemedText style={globalStyle.title}>{item.title}</ThemedText>
+									<ThemedText>Written by {item.author}</ThemedText>
+
+							</ThemedCard>
+						</Animated.View>
+
 					</Pressable>
 				)}
 			/>
@@ -56,5 +93,8 @@ const styles = StyleSheet.create({
 		paddingLeft: 14,
 		borderLeftColor: Colors.primary,
 		borderLeftWidth: 5
+	},
+	pressed: {
+		backgroundColor: 'red'
 	}
 })
